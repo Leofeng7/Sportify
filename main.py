6bdb9db7199d3cf1c2965001e36c22d9b7d8c9f3
rijulrr <rijulr@tamu.edu>
2023-11-29 17:33:24 -0600

    Add creds support for multiple users

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
+import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 def load_credentials(file_path):
-    credentials = {}
     with open(file_path, 'r') as file:
-        for line in file:
-            if line.startswith('/'):
-                continue
-            key, value = line.strip().split('=', 1)
-            credentials[key] = value
+        credentials = json.load(file)
     return credentials
 
-credentials = load_credentials('credentials.txt')
+def create_spotify_oauth(user_credentials):
+    return SpotifyOAuth(client_id=user_credentials['SPOTIFY_CLIENT_ID'],
+                        client_secret=user_credentials['SPOTIFY_CLIENT_SECRET'],
+                        redirect_uri=user_credentials['REDIRECT_URI'],
+                        scope="playlist-read-private")
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=credentials['SPOTIFY_CLIENT_ID'],
-                                               client_secret=credentials['SPOTIFY_CLIENT_SECRET'],
-                                               redirect_uri=credentials['REDIRECT_URI'],
-                                               scope="playlist-read-private"))
-def get_playlist_tracks(username, playlist_id):
+def get_playlist_tracks(sp, username, playlist_id):
     results = sp.user_playlist_tracks(username, playlist_id)
     tracks = results['items']
     while results['next']:
@@ -28,19 +24,33 @@ def get_playlist_tracks(username, playlist_id):
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    username = '1295463866'
-    playlist_id = '2b8pyKWkXM7PvPtXZ6aNLH'
-    tracks = get_playlist_tracks(username, playlist_id)
-    return render_template('index.html', tracks=tracks)
+@app.route("/<username>")
+def index(username):
+    credentials = load_credentials('credentials.json')
+    user_credentials = credentials.get(username)
+    
+    if not user_credentials:
+        abort(404)
 
-@app.route("/playlist/daily-mix-1")
-def daily_mix_1():
-    username = '1295463866'
-    playlist_id = '2b8pyKWkXM7PvPtXZ6aNLH'
-    tracks = get_playlist_tracks(username, playlist_id)
-    return render_template('playlist.html', tracks=tracks)
+    sp_oauth = create_spotify_oauth(user_credentials)
+    sp = spotipy.Spotify(auth_manager=sp_oauth)
+    playlist_id = '2b8pyKWkXM7PvPtXZ6aNLH'  
+    tracks = get_playlist_tracks(sp, user_credentials['SPOTIFY_USERNAME'], playlist_id)
+    return render_template('index.html', tracks=tracks, username=username)
+
+@app.route("/<username>/playlist/daily-mix-1")
+def daily_mix_1(username):
+    credentials = load_credentials('credentials.json')
+    user_credentials = credentials.get(username)
+    
+    if not user_credentials:
+        abort(404)
+
+    sp_oauth = create_spotify_oauth(user_credentials)
+    sp = spotipy.Spotify(auth_manager=sp_oauth)
+    playlist_id = '2b8pyKWkXM7PvPtXZ6aNLH'  
+    tracks = get_playlist_tracks(sp, user_credentials['SPOTIFY_USERNAME'], playlist_id)
+    return render_template('playlist.html', tracks=tracks, username=username)
 
 if __name__ == "__main__":
     app.run(debug=False)
